4a27a7f99e88769f6a341e145c7a5a3b4e1f5a51
haykal-fe-verd <haykal@verd.net.id>
2024-02-22 22:43:11 +0700

    fix: remove unnecessary resource

--- resources/js/pages/profile/partials/update-password-form.jsx
+++ resources/js/pages/profile/partials/update-password-form.jsx
@@ -4,9 +4,6 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import InputError from "@/components/input-error";
-import InputLabel from "@/Components/InputLabel";
-import PrimaryButton from "@/Components/PrimaryButton";
-import TextInput from "@/Components/TextInput";
 import { useForm } from "@inertiajs/react";
 import { Transition } from "@headlessui/react";
 
